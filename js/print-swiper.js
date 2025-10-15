c393cbf03a9aa732b308aa1a1922be8fbbf164dc
gohayng <hometown1129@tidesquare.com>
2025-10-16 03:40:54 +0900

    fix : swiper 다음 버튼 누르면 계속 넘어가는 오류 수정하다

--- js/print-swiper.js
+++ js/print-swiper.js
@@ -2,6 +2,9 @@
 const swiper = new Swiper(".print-swiper", {
   // Optional parameters
   direction: "horizontal",
+  // loop: true, // 무한 루프 활성화
+  slidesPerView: "auto",
+  // centeredSlides: true, // 슬라이드를 중앙에 정렬
   // autoplay: {
   //   delay: 3000,
   //   disableOnInteraction: false,
